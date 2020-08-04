de58cd57b06d2808b22b9dae45d2745bbadd6669
dth <dth@dthlabs.com>
2020-08-04 20:01:22 +0200

    finishing landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -30,8 +30,8 @@ function Landing(){
             Estudar
           </a>
           <a href="/" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar aula"/>
-            Dar aula
+            <img src={giveClassesIcon} alt="Dar aulas"/>
+            Dar aulas
           </a>
         </div>
 
