68d7e93b891f2cd93daa892026f9ab2f27822580
dth <dth@dthlabs.com>
2020-08-05 11:44:26 +0200

    express init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log("hello world!");
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  const users = [
+    { name: 'John', age: 25 },
+    { name: 'Mary', age: 21 }
+  ];
+
+  return response.json(users);
+});
+
+app.listen(3333);
