ee4bf61402e32f19afb774184674e8e8d5a5ea90
dth <dth@dthlabs.com>
2020-08-16 15:46:35 +0200

    filters

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView, Text, TextInput } from 'react-native';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
@@ -7,9 +7,42 @@ import TeacherItem from '../../components/TeacherItem';
 import styles from './styles';
 
 function TeacherList() {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Formateurs disponibles" />
+      <PageHeader title="Formateurs disponibles">
+        { isFiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Sujet</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Quel sujet?"
+              placeholderTextColor="#C1BCCC"
+            />
+            
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Jour de la semaine</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Quel jour?"
+                  placeholderTextColor="#C1BCCC"
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horaire</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Quel horaire?"
+                  placeholderTextColor="#C1BCCC"
+                />
+              </View>
+            </View>
+          </View>
+        )}
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -7,8 +7,35 @@ const styles = StyleSheet.create({
   },
 
   teacherList: {
-    marginTop: -60,
-    padding: 16,
+    marginTop: -40,
+  },
+
+  searchForm: {
+    marginBottom: 8,
+  },
+
+  label: {
+    color: '#D4C2FF',
+    fontFamily: 'Poppins_400Regular'
+  },
+
+  inputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  inputBlock: {
+    width: '48%',
+  },
+
+  input: {
+    height: 54,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 16
   }
 });
 
