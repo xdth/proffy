6b0de568160ed5ad52e4cafbc3291628e6f0d7f6
dth <dth@dthlabs.com>
2020-08-16 22:42:52 +0200

    techer list style

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
@@ -10,9 +10,10 @@ import styles from './styles';
 
 interface PageHeaderProps {
   title: string;
+  headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
   const { navigate } = useNavigation();
 
   function handleGoBack() {
@@ -29,8 +30,11 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
         <Image source={logoImg} resizeMode="contain" style={{width: 50, height: 50}} />
       </View>
 
-      <Text style={styles.title}>{title}</Text>
-
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
+      
       {children}
     </View>
   );
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
@@ -9,9 +11,20 @@ import styles from './styles';
 function TeacherList() {
   const [isFiltersVisible, setIsFiltersVisible] = useState(false);
 
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Formateurs disponibles">
+      <PageHeader
+        title="Formateurs disponibles"
+        headerRight={(
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name="filter" size={20} color='#FFF' />
+          </BorderlessButton>
+        )}
+      >
         { isFiltersVisible && (
           <View style={styles.searchForm}>
             <Text style={styles.label}>Sujet</Text>
@@ -40,6 +53,9 @@ function TeacherList() {
                 />
               </View>
             </View>
+            <RectButton style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Filtrer</Text>
+            </RectButton>
           </View>
         )}
       </PageHeader>
